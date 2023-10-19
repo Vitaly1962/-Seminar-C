f09ac74955da32459f96c6155ce82f6c47ae3a5b
Koltsov Vitaly <koltsov2007@yandex.ru>
2023-10-20 00:09:19 +0500

    DZ 4

--- DZ_3.4/Program.cs
+++ DZ_3.4/Program.cs
@@ -3,8 +3,10 @@
 Console.WriteLine($"{msg}");
 return Convert.ToInt32(Console.ReadLine());
 }
+
+
 int number = Prompt("Введите трехзначное число");
-Console.WriteLine(number);
+
 if (number >=100 && number <1000)
 {
 int mid = (number % 100 - number % 10) / 10;
@@ -12,3 +14,5 @@ Console.WriteLine($"Вторая цифра числа {number} является
 }
 else
 Console.WriteLine("Это число не трехзначное");
+
+
---
+++ Program.cs
@@ -0,0 +1,2 @@
+﻿int n = 123456789;
+int digitCount = (int)Math.Log10(n) + 1;
